e764bd11fb1909080c99b556021e2169af5c3acc
Administrator <baptiste@riegeldev.fr>
2020-01-01 10:29:42 +0100

    fix the test case status component

--- ui/components/testCaseState.vue
+++ ui/components/testCaseState.vue
@@ -1,20 +1,22 @@
 <template>
     <span>
         <span v-if="displayCurrentState">
-            <md-icon v-if="testState === statuses.TODO">pause_circle_outline</md-icon>
-            <md-icon v-if="testState === statuses.IN_PROGRESS">loop</md-icon>
-            <md-icon v-if="testState === statuses.FAILED">thumb_down</md-icon>
-            <md-icon v-if="testState === statuses.BLOCKED">block</md-icon>
-            <md-icon v-if="testState === statuses.SUCCESS">thumb_up</md-icon>
+            <md-icon v-if="curState === statuses.TODO">pause_circle_outline</md-icon>
+            <md-icon v-if="curState === statuses.IN_PROGRESS">loop</md-icon>
+            <md-icon v-if="curState === statuses.FAILED">thumb_down</md-icon>
+            <md-icon v-if="curState === statuses.BLOCKED">block</md-icon>
+            <md-icon v-if="curState === statuses.SUCCESS">thumb_up</md-icon>
         </span>
         <span v-if="displayStateSwitch">
-            <md-select v-model="testState" name="testStateSel" id="testStateSel" class="test-state-selector" @md-selected="changeTestStatus">
-                <md-option :value="statuses.TODO"><md-icon>pause_circle_outline</md-icon>To do</md-option>
-                <md-option :value="statuses.IN_PROGRESS"><md-icon>loop</md-icon>In progress</md-option>
-                <md-option :value="statuses.FAILED"><md-icon>thumb_down</md-icon>Failed</md-option>
-                <md-option :value="statuses.BLOCKED"><md-icon>block</md-icon>Blocked</md-option>
-                <md-option :value="statuses.SUCCESS"><md-icon>thumb_up</md-icon>Success</md-option>
-            </md-select>
+            <md-field>
+                <md-select v-model="curState" name="testStateSel" id="testStateSel" class="test-state-selector" @md-selected="changeTestStatus">
+                    <md-option :value="statuses.TODO">To do</md-option>
+                    <md-option :value="statuses.IN_PROGRESS">In progress</md-option>
+                    <md-option :value="statuses.FAILED">Failed</md-option>
+                    <md-option :value="statuses.BLOCKED">Blocked</md-option>
+                    <md-option :value="statuses.SUCCESS">Success</md-option>
+                </md-select>
+            </md-field>
         </span>
     </span>
 </template>
@@ -29,20 +31,18 @@
         },
         data: function () {
             return {
-                statuses: TEST_CASE_STATUSES
+                statuses: TEST_CASE_STATUSES,
+                curState: this.testState
             }
         },
         methods: {
             changeTestStatus(newStatus) {
+            	this.curState = newStatus;
                 this.$emit('change-test-status', newStatus);
             }
         }
     }
 </script>
 <style scoped lang="scss">
-.test-state-selector {
-    .md-list-item-text {
-        display: block;
-    }
-}
+
 </style>
