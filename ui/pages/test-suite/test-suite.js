16dc91113cf67f1ab7679d6ccf96687a29fa4e85
Administrator <baptiste@riegeldev.fr>
2021-06-03 11:13:04 +0200

    Add a spirit of dashboard on test suite page

--- ui/pages/test-suite/test-suite.js
+++ ui/pages/test-suite/test-suite.js
@@ -7,6 +7,37 @@ import TestSuiteHistory from '../../components/testSuiteHistory.vue';
 import {fileTreeUtils} from '../../components/fileTree.js';
 import BreadCrumb from '../../components/breadCrumb.vue';
 import {breadCrumbEventBus} from '../../components/breadCrumb.vue';
+import { D3PieChart } from 'vue-d3-charts';
+
+const DEFAULT_TEST_SUITE_CHART_CONFIG = {
+	key: false,
+	radius: {
+		inner: false,
+		outter: false,
+		padding: 0,
+		round: 0,
+	},
+	value: false,
+	color: {
+		key: false,
+		keys: false,
+		scheme: false,
+		current: "#1f77b4",
+		default: "#AAA",
+		axis: "#000",
+	},
+	currentKey: false,
+	margin: {
+		top: 40,
+		right: 40,
+		bottom: 40,
+		left: 40,
+	},
+	transition: {
+		duration: 100,
+		ease: "easeLinear",
+	},
+};
 
 const TEST_SUITE_PATH = '/api/test-suites/';
 export default {
@@ -15,13 +46,18 @@ export default {
 		MainLayout,
 		FileTree,
 		TestSuiteHistory,
-		BreadCrumb
+		BreadCrumb,
+		D3PieChart
 	},
 	data() {
 		return {
 			testSuite: null,
 			openedTestCase: null,
-			testsTree: null
+			testsTree: null,
+			testSuiteStatusChart: null,
+			testSuiteStatusChartConfig: {},
+			testSuiteProgressChart: null,
+			testSuiteProgressChartConfig: {}
 		}
 	},
 	async mounted() {
@@ -63,6 +99,8 @@ export default {
 							})
 						}
 					});
+					this.testSuiteStatusChartData();
+					this.testSuiteProgressionChartData();
 				}
 				if (testCaseId) {
 					this.openTest({path: decodeURIComponent(testCaseId)});
@@ -90,6 +128,107 @@ export default {
 		},
 		updateTestCase() {
 			return this.initTestSuite();
+		},
+		testSuiteProgressionChartData() {
+			const tests = this.testSuite.tests;
+			const total = tests.length;
+
+			const statuses = {
+				TODO: 0,
+				IN_PROGRESS: 1,
+				BLOCKED: 3,
+				SUCCESS: 4,
+				FAILED: 5
+			};
+
+			function getTestsStatusPercent(status, total) {
+				const part = tests.filter(test => test.status === status).length;
+				return Math.round((part / total) * 100);
+			}
+
+			const totalTodo = getTestsStatusPercent(statuses.TODO, total);
+			const totalProgress = getTestsStatusPercent(statuses.IN_PROGRESS, total);
+			const totalBlocked = getTestsStatusPercent(statuses.BLOCKED, total);
+			const totalSuccess = getTestsStatusPercent(statuses.SUCCESS, total);
+			const totalFailed = getTestsStatusPercent(statuses.FAILED, total);
+
+			this.testSuiteProgressChart = [{
+				name: "Finished",
+				total: totalSuccess + totalFailed
+			}, {
+				name: "To do",
+				total: totalTodo
+			}, {
+				name: "In progress",
+				total: totalBlocked + totalProgress
+			}];
+			// colors based on https://material.io/resources/color/#!/?view.left=0&view.right=0 picked on material palette on the Blue row
+			this.testSuiteProgressChartConfig = Object.assign({}, DEFAULT_TEST_SUITE_CHART_CONFIG, {
+				key: 'name',
+				value: 'total',
+				color: {
+					keys: {
+						'Finished': '#1565c0', // 800
+						'To do': '#bbdefb', // 100
+						'In progress': '#42a5f5' // 400
+					}
+				}
+			});
+		},
+		testSuiteStatusChartData() {
+			const tests = this.testSuite.tests;
+			const total = tests.length;
+
+			const statuses = {
+				TODO: 0,
+				IN_PROGRESS: 1,
+				BLOCKED: 3,
+				SUCCESS: 4,
+				FAILED: 5
+			};
+
+			function getTestsStatusPercent(status, total) {
+				const part = tests.filter(test => test.status === status).length;
+				return Math.round((part / total) * 100);
+			}
+
+			const totalTodo = getTestsStatusPercent(statuses.TODO, total);
+			const totalProgress = getTestsStatusPercent(statuses.IN_PROGRESS, total);
+			const totalBlocked = getTestsStatusPercent(statuses.BLOCKED, total);
+			const totalSuccess = getTestsStatusPercent(statuses.SUCCESS, total);
+			const totalFailed = getTestsStatusPercent(statuses.FAILED, total);
+
+			this.testSuiteStatusChart = [{
+				name: "To do",
+				total: totalTodo
+			}, {
+				name: "In progress",
+				total: totalProgress
+			}, {
+				name: "Blocked",
+				total: totalBlocked
+			}, {
+				name: "Success",
+				total: totalSuccess
+			}, {
+				name: "Failed",
+				total: totalFailed
+			}];
+
+			this.testSuiteStatusChartConfig = Object.assign({}, DEFAULT_TEST_SUITE_CHART_CONFIG, {
+				key: 'name',
+				value: 'total',
+				color: {
+					// colors based on https://material.io/resources/color/#!/?view.left=0&view.right=0 picked on material palette on the third column (200)
+					keys: {
+						'To do': '#b0bec5', // Blue Grey
+						'In progress': '#90caf9', // Blue
+						'Blocked': '#ffcc80', // Orange
+						'Success': '#a5d6a7', // Green
+						'Failed': '#ef9a9a' // Red
+					}
+				}
+			});
 		}
 	}
 };
