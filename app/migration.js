7c9e993e5e2c9f48f066f38eb877ee0f4e4875e5
Administrator <baptiste@riegeldev.fr>
2021-06-18 08:51:05 +0200

    Add a metadata

--- app/migration.js
+++ app/migration.js
@@ -4,14 +4,36 @@ const path = require('path');
 
 const dbConnector = require('./db/db-connector');
 
+const APPLICATION_METADATA_ID = 'application-metadata';
+
 async function setMetadata() {
 	const coll = await dbConnector.getCollection(dbConnector.DB_TABLES.METADATA);
 	const pjson = require(path.join(__dirname, '../package.json'));
-	await coll.insertOne({
-		applicationVersion: pjson.version
-	});
+
+	const existingMetadata = await getMetadata();
+	if (!existingMetadata) {
+		await coll.insertOne({
+			_id: APPLICATION_METADATA_ID,
+			applicationVersion: pjson.version
+		});
+	} else {
+		await coll.updateOne({_id: APPLICATION_METADATA_ID}, {
+			$set: Object.assign({}, existingMetadata, {
+				applicationVersion: pjson.version
+			})
+		});
+	}
 }
 
+async function getMetadata() {
+	const coll = await dbConnector.getCollection(dbConnector.DB_TABLES.METADATA);
+	const cursor = await coll.find();
+	const itemsCount = await cursor.count();
+	if (itemsCount > 0){
+		return await cursor.toArray()[0];
+	}
+	return null;
+}
 
 async function startMigration() {
 	await setMetadata();
